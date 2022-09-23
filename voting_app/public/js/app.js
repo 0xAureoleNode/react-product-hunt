258dc83d315c8483f629adf2b70c000bc1634c05
”0xAureoleNode“ <aureolenode@gmail.com>
2022-09-23 12:08:19 +0800

    react-functional-component: for building product

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,7 +1,27 @@
 const ProductList = () => {
   return (
     <div className="ui unstackable items">
-      hello react, I'm basic functional component
+      <Product />
+    </div>
+  );
+};
+
+const Product = () => {
+  return (
+    <div className="item">
+      <div className="image">
+        <img src="images/products/image-aqua.png" />
+      </div>
+      <div className="middle aligned content">
+        <div className="description">
+          <a>Fort Knight</a>
+          <p>Authentic renaissance actors, delivered in just two week\ s.</p>
+        </div>
+        <div className="extra">
+          <span>Submitted by:</span>
+          <img className="ui avatar image" src="images/avatars/daniel.jpg" />
+        </div>
+      </div>
     </div>
   );
 };

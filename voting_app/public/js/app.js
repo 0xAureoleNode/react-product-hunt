ef958431f83ddf699e0b04d6df57bf60f8702be9
”0xAureoleNode“ <aureolenode@gmail.com>
2022-09-23 12:43:51 +0800

    react-functional-component  commit for Making product data-driven

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,25 +1,41 @@
 const ProductList = () => {
-  return (
-    <div className="ui unstackable items">
-      <Product />
-    </div>
-  );
+  const products = Seed.products.sort((a, b) => b.votes - a.votes);
+
+  const productComponents = products.map((product) => (
+    <Product
+      key={'product-' + product.id}
+      id={product.id}
+      title={product.title}
+      description={product.description}
+      url={product.url}
+      votes={product.votes}
+      submitterAvatarUrl={product.submitterAvatarUrl}
+      productImageUrl={product.productImageUrl}
+    />
+  ));
+  return <div className="ui unstackable items">{productComponents}</div>;
 };
 
-const Product = () => {
+const Product = (props) => {
   return (
     <div className="item">
       <div className="image">
-        <img src="images/products/image-aqua.png" />
+        <img src={props.productImageUrl} />
       </div>
       <div className="middle aligned content">
+        <div className="header">
+          <a>
+            <i className="large caret up icon" />
+          </a>
+          {props.votes}
+        </div>
         <div className="description">
-          <a>Fort Knight</a>
-          <p>Authentic renaissance actors, delivered in just two week\ s.</p>
+          <a href={props.url}>{props.title}</a>
+          <p>{props.description}</p>
         </div>
         <div className="extra">
           <span>Submitted by:</span>
-          <img className="ui avatar image" src="images/avatars/daniel.jpg" />
+          <img className="ui avatar image" src={props.submitterAvatarUrl} />
         </div>
       </div>
     </div>

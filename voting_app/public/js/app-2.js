eb4e70310625749a409e9628c68e4d3998976415
”0xAureoleNode“ <aureolenode@gmail.com>
2022-09-23 12:02:42 +0800

    commit for building product

--- voting_app/public/js/app-2.js
+++ voting_app/public/js/app-2.js
@@ -1,8 +1,7 @@
-
 class ProductList extends React.Component {
   render() {
     return (
-      <div className='ui unstackable items'>
+      <div className="ui unstackable items">
         <Product />
       </div>
     );
@@ -12,21 +11,18 @@ class ProductList extends React.Component {
 class Product extends React.Component {
   render() {
     return (
-      <div className='item'>
-        <div className='image'>
-          <img src='images/products/image-aqua.png' />
+      <div className="item">
+        <div className="image">
+          <img src="images/products/image-aqua.png" />
         </div>
-        <div className='middle aligned content'>
-          <div className='description'>
+        <div className="middle aligned content">
+          <div className="description">
             <a>Fort Knight</a>
             <p>Authentic renaissance actors, delivered in just two weeks.</p>
           </div>
-          <div className='extra'>
+          <div className="extra">
             <span>Submitted by:</span>
-            <img
-              className='ui avatar image'
-              src='images/avatars/daniel.jpg'
-            />
+            <img className="ui avatar image" src="images/avatars/daniel.jpg" />
           </div>
         </div>
       </div>
@@ -34,7 +30,4 @@ class Product extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
+ReactDOM.render(<ProductList />, document.getElementById('content'));
